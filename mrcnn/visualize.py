fef23ed19e349bf0acb31054d8dcfd3d425e2f2e
ncywong <nicholaswong65656ster@gmail.com>
2020-11-04 10:59:31 +0000

    savefig

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -582,4 +582,5 @@ def save_masked_image(image, boxes, masks, class_ids, class_names,
             verts = np.fliplr(verts) - 1
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
     plt.savefig(img_path)
